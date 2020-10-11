53ca363514e496eab7987df4639cad32454e5852
chikuwa111 <take0shun1111@gmail.com>
2020-10-11 22:56:19 +0900

    feat: Playwrightのサンプルコードを実装

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { chromium } from "playwright";
+
+const { EXECUTABLE_PATH } = process.env;
+
+async function main() {
+  const browser = await chromium.launch({ executablePath: EXECUTABLE_PATH });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://example.com");
+  const title = await page.evaluate(() => document.title);
+  console.log(title);
+  await browser.close();
+}
+
+main();
